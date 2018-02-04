cf6a6a5a9f90e9d4f5b327d012fe8fb1e1d266a0
Tomas Prochazka <tomas.gass@gmail.com>
2018-02-04 22:47:51 +0100

    Indents editation

--- src/ftpcheck.cpp
+++ src/ftpcheck.cpp
@@ -3,37 +3,37 @@
 using namespace curlpp::options;
 
 void download(string fileurl, string outfilename){
-      curlpp::Cleanup myCleanup;
-      ofstream os(outfilename.c_str(), ios::binary);
-		  os << curlpp::options::Url(fileurl);
+    curlpp::Cleanup myCleanup;
+    ofstream os(outfilename.c_str(), ios::binary);
+    os << curlpp::options::Url(fileurl);
 }
 
 string getContent(string stringurl){
 	try
   	{
-    		curlpp::Cleanup myCleanup;
+        curlpp::Cleanup myCleanup;
 		curlpp::Easy myRequest;
 
-    		// Set the URL.
-    		myRequest.setOpt<Url>(stringurl);
+    	// Set the URL.
+    	myRequest.setOpt<Url>(stringurl);
 
-    		std::ostringstream os;
-    		curlpp::options::WriteStream ws(&os);
-    		myRequest.setOpt(ws);
-    		os << myRequest;
-    		string content =  os.str();
-    		return content;
+    	std::ostringstream os;
+    	curlpp::options::WriteStream ws(&os);
+    	myRequest.setOpt(ws);
+    	os << myRequest;
+    	string content =  os.str();
+    	return content;
   	}
 
   	catch(curlpp::RuntimeError & e)
   	{
-    		std::cout << e.what() << std::endl;
+    	std::cout << e.what() << std::endl;
 		return "";
   	}
 
   	catch(curlpp::LogicError & e)
   	{
-    		std::cout << e.what() << std::endl;
+    	std::cout << e.what() << std::endl;
 		return "";
   	}
 }
--- src/strjob.cpp
+++ src/strjob.cpp
@@ -10,17 +10,17 @@ string edited(string s){
 }
 
 vector<string> getLines(string file){
-  ifstream in;
-  in.open(file.c_str());
-  vector<string> lines;
-  for(int i(0);i!=1;){
+    ifstream in;
+    in.open(file.c_str());
+    vector<string> lines;
+    for(int i(0);i!=1;){
         string line;
         getline(in,line);
         if(in.eof()) i = 1;
         else lines.push_back(line); 
     }
-  in.close();
-  return lines;
+    in.close();
+    return lines;
 }
 
 void write(ofstream & out, vector<string> lines){
@@ -33,7 +33,7 @@ void write(ofstream & out, vector<string> lines){
 string notOdd(string s){
     string result = "";
     for(int i(1); i<s.size(); i+=2){
-      result += s[i];
+        result += s[i];
     }
     return result;
 }
@@ -48,15 +48,15 @@ void writeCorrect(ofstream & out, vector<string> lines){
 }
 
 vector<string> splitlines(string text){
-  vector<string> res;
-  string a="";
-  for(int i(0); i!=text.size();++i){
-    if(text[i]!='\r' && text[i]!='\n') a+=text[i];
-    else if(a.size()>0) {
-        res.push_back(a);
-        a = "";
+    vector<string> res;
+    string a="";
+    for(int i(0); i!=text.size();++i){
+        if(text[i]!='\r' && text[i]!='\n') a+=text[i];
+        else if(a.size()>0) {
+            res.push_back(a);
+            a = "";
+        }
     }
-  }
-  res.push_back(a);
-  return res;
+    res.push_back(a);
+    return res;
 }
